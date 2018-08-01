de6de3d184373c8885b6d34beb95b97c4f0ab5ef
PC de Charlène <Charlene@MacBook-Pro-de-PC.local>
2018-08-01 22:34:42 +0200

    product section working

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function openPage(pageName,elmnt,color) {
     elmnt.style.backgroundColor = color;
 
 }
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
@@ -26,6 +27,7 @@ document.querySelector("svg").addEventListener("mouseover", animate);
     function animateBack(){
         document.querySelector("line").style.strokeDashoffset = 100;
     }
+
 
 let data;
 // DASHBOARD SECTION DECLARATION 
@@ -54,6 +56,7 @@ setInterval(this.getMyData.bind(this), 1 * 1000);
 function getMyData(){
     data = FooBar.getData();
     const myJson = JSON.parse(data);
+    console.log(myJson);
     numInQueue.innerHTML = myJson.queue.length;
     numBeingServed.innerHTML = myJson.serving.length;
     numStaff.innerHTML = myJson.bartenders.length;
@@ -64,35 +67,33 @@ function getMyData(){
     numProduct.innerHTML = myJson.beertypes.length;
 
     let beerData = myJson.beertypes;
-    console.log(beerData);
-    //console.log(beerData); 
     beerData.forEach(function(beertype){
         if(beerTypes.findIndex((b) => b.name == beertype.name) <= -1){
             beerTypes.push(beertype);
             let cloneBeer = productTemplate.cloneNode(true);
             let beerName = cloneBeer.querySelector(".beerName");
-            let beerAppearance = cloneBeer.querySelector(".appearText");            
             beerName.textContent = beertype.name;
-            beerAppearance.textContent = beerData.appearance;
+            let beerAppear = cloneBeer.querySelector(".appearText");
+            beerAppear.textContent = beertype.description.appearance;
+            let beerAroma = cloneBeer.querySelector(".aromaText");
+            beerAroma.textContent = beertype.description.aroma;
+            let beerFlavor = cloneBeer.querySelector(".flavorText");
+            beerFlavor.textContent = beertype.description.flavor;
+            let beerMouth = cloneBeer.querySelector(".mouthText");
+            beerMouth.textContent = beertype.description.mouthfeel;
+            let beerOverall = cloneBeer.querySelector(".overallText");
+            beerOverall.textContent = beertype.description.overallImpression;
+            let beerImg = cloneBeer.querySelector("img");
+            beerImg.src="images/" + beertype.label;
             beerSection.appendChild(cloneBeer);
 
 
-
         }
-        if(beerTypes.findIndex((b) => b.appearance == beertype.appearance) <= -1){
-            beerTypes.push(beertype);
-            let cloneBeer = productTemplate.cloneNode(true);
-            let beerAppearance = cloneBeer.querySelector(".appearText");            
-            beerAppearance.textContent = beerData.appearance;
-            beerSection.appendChild(cloneBeer);}
     });
- 
-
 
 
 
 };
-
 // Get Beer data
     
 
